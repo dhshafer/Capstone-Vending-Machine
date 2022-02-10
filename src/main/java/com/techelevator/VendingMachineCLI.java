8ac31d4356dd9e840523b7998db8188fb3646e59
David Shafer <dhshafer.73@gmail.com>
2022-02-10 11:59:55 -0600

    Added validateInput Method

--- src/main/java/com/techelevator/VendingMachineCLI.java
+++ src/main/java/com/techelevator/VendingMachineCLI.java
@@ -3,6 +3,7 @@ package com.techelevator;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.math.BigDecimal;
+import java.sql.SQLOutput;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -24,6 +25,11 @@ public class VendingMachineCLI {
 		VendingMachine vendingMachine = new VendingMachine();
 		vendingMachine.readInData();
 		vendingMachine.printDisplay();
+		Scanner userInput = new Scanner(System.in);
+
+		String[] expectedInputs = new String[] {"1", "2", "3", "4"};
+		String message = "\nMain Menu \n1. Display items \n2. Purchase items \n3. Exit\n";
+		String value = validateInput(expectedInputs, userInput, message, "Please enter 1, 2, or 3.");
 	}
 
 	public void displayWelcomeMessage(){
@@ -33,6 +39,17 @@ public class VendingMachineCLI {
 		System.out.println("");
 	}
 
-
+	public String validateInput(String[] expected, Scanner input, String message, String invalidMessage){
+		while (true) {
+			System.out.printf(message + "\n");
+			String value = input.nextLine().trim();
+			for (String item : expected){
+				if (item.equals(value)){
+					return value;
+				}
+			}
+			System.out.println(invalidMessage);
+		}
+	}
 
 }
